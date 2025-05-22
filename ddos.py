7106311fd6ef68418f29379623741ec7f3edaeb3
NobilSec <113498887+NobilSec@users.noreply.github.com>
2025-05-22 06:50:57 -0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -609,27 +609,25 @@ def main():
         clear()
         banner()
         print("""
-        
-                    \033[33m:::!~!!!!!:.\033[0m
-                  \033[33m.xUHWH!! !!?M88WHX:.\033[0m
-                \033[33m.X*#M@$!!  !X!M$$$$$$WWx:.\033[0m
-               \033[33m:!!!!!!?H! :!$!$$$$$$$$$$8X:\033[0m
-              \033[33m!!~  ~:~!! :~!$!#$$$$$$$$$$8X:\033[0m
-             \033[33m:!~::!H!<   ~.U$X!?R$$$$$$$$MM!\033[0m
-             \033[33m~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!\033[0m
-               \033[33m!:~~~ .:!M"T#$$$$WX??#MRRMMM!\033[0m
-               \033[33m~?WuxiW*`   `"#$$$$8!!!!??!!!\033[0m
-             \033[33m:X- M$$$$       `"T#$T~!8$WUXU~\033[0m
-            \033[33m:%`  ~#$$$m:        ~!~ ?$$$$$$\033[0m
-          \033[33m:!`.-   ~T$$$$8xx.  .xWW- ~""##*"\033[0m
-\033[33m.....   -~~:<` !    ~?T#$$@@W@*?$$      /`\033[0m
-\033[33mW$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :\033[0m
-\033[33m#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`\033[0m
-\033[33m:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~\033[0m
-\033[33m.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `\033[0m
-\033[33mWi.~!X$?!-~    : ?$$$B$Wu("**$RM!          Alien C2 Server [ L7 & L4 ]\033[0m
-\033[33m$R@i.~~ !     :   ~$$$$$B$$en:``\033[0m
-\033[33m?MXT@Wx.~    :     ~"##*$$$$M~\033[0m
+                    \033[31m_                      _______                      _
+  _dMMMb._              .adOOOOOOOOOba.              _,dMMMb_
+ dP'  ~YMMb            dOOOOOOOOOOOOOOOb            aMMP~  `Yb
+ V      ~"Mb          dOOOOOOOOOOOOOOOOOb          dM"~      V
+          `Mb.       dOOOOOOOOOOOOOOOOOOOb       ,dM'
+           `YMb._   |OOOOOOOOOOOOOOOOOOOOO|   _,dMP'
+      __     `YMMM| OP'~"YOOOOOOOOOOOP"~`YO |MMMP'     __
+    ,dMMMb.     ~~' OO     `YOOOOOP'     OO `~~     ,dMMMb.
+ _,dP~  `YMba_      OOb      `OOO'      dOO      _aMMP'  ~Yb._
+
+             `YMMMM\`OOOo     OOO     oOOO'/MMMMP'
+     ,aa.     `~YMMb `OOOb._,dOOOb._,dOOO'dMMP~'       ,aa.
+   ,dMYYMba._         `OOOOOOOOOOOOOOOOO'          _,adMYYMb.
+  ,MP'   `YMMba._      OOOOOOOOOOOOOOOOO       _,adMMP'   `YM.
+  MP'        ~YMMMba._ YOOOOPVVVVVYOOOOP  _,adMMMMP~       `YM
+  YMb           ~YMMMM\`OOOOI`````IOOOOO'/MMMMP~           dMP
+   `Mb.           `YMMMb`OOOI,,,,,IOOOO'dMMMP'           ,dM'
+     `'                  `OObNNNNNdOO'                   `'
+                           `~OOOOO~'           Alien C2 Server [ L7 & L4 ]\033[0m
         ╔══════════════════════════════════════════════════════════════════════════
         ║                      ADVANCED NETWORK STRESS TESTING                      
         ║                        SECURITY RESEARCH TOOLKIT                          
